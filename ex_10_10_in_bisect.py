0519911baa67e7b28ae9535a302513ef7fc608b1
and-sang <and-sang@outlook.com>
2020-04-02 19:07:20 +0200

    removed scaffolding for use in later exercises

--- ex_10_10_in_bisect.py
+++ ex_10_10_in_bisect.py
@@ -9,10 +9,10 @@ def in_bisect(li, el):
     if el == t[bi]:
         return True
     elif el < t[bi]:
-        print(f'el: {el}, t[bi]: {t[bi]}\nt[bi:]: {t[:bi]}')
+        # print(f'el: {el}, t[bi]: {t[bi]}\nt[bi:]: {t[:bi]}')
         return in_bisect(t[:bi], el)    # last element of slice not included
     else:
-        print(f'el: {el}, t[bi]: {t[bi]}\nt[bi+1:]: {t[bi+1:]}')
+        # print(f'el: {el}, t[bi]: {t[bi]}\nt[bi+1:]: {t[bi+1:]}')
         return in_bisect(t[bi+1:], el)  # first element of slice included, but already checked
     return False
 
